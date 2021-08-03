71dfba2f3d3c39f73932724327ed7d366ba9f583
HR Williams <halrubinwilliams@gmail.com>
2021-08-03 15:28:23 -0700

    Fix text

--- src/components/NewTicketPrompt3.js
+++ src/components/NewTicketPrompt3.js
@@ -3,7 +3,7 @@ import React from "react";
 function NewTicketPrompt3(props){
   return(
     <React.Fragment>
-      <h3>"Have you asked another pair for help?"</h3>
+      <h3>Have you spent 15 minutes going through through the problem documenting every step?</h3>
     </React.Fragment>
   );
 }
